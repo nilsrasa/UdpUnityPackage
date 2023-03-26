f00b34db1e2347e3c7d5eedd5afd08bb6b82a1bc
nilsrasa <32563656+nilsrasa@users.noreply.github.com>
2023-03-26 15:48:35 +0200

    Update ExoUdpHost.cs
    Changed to match the new exo which only outputs elbow value

--- Assets/Udp/Scripts/ExoUdpHost.cs
+++ Assets/Udp/Scripts/ExoUdpHost.cs
@@ -8,10 +8,8 @@ namespace Exo
     public class ExoUdpHost : UdpHost
     {
         public UnityEvent<ExoEntry> OnElbowValue;
-        public UnityEvent<ExoEntry> OnWristValue;
 
         public float ElbowValue => _elbowValue;
-        public float WristValue => _wristValue;
 
         private float _elbowValue, _wristValue;
 
@@ -23,40 +21,27 @@ namespace Exo
             var data = message.Split(',');
             var unitytime = DateTime.Now.ToString("HH:mm:ss.ffffff");
 
-            if (data.Length == 3)
+            if (data.Length == 2)
             {
-                //Parse the first csv which is the elbow angle
+                //Parse the elbow angle value
                 if (float.TryParse(data[0], NumberStyles.Any, CultureInfo.InvariantCulture, out _elbowValue))
                 {
                     var entry = new ExoEntry
                     {
                         Value = _elbowValue,
-                        UdpTimestamp = data[2],
+                        UdpTimestamp = data[1],
                         UnityTimestamp = unitytime
                     };
 
                     OnElbowValue.Invoke(entry);
                 }
-
-                //Parse the second csv which is the wrist angle
-                if (float.TryParse(data[1], NumberStyles.Any, CultureInfo.InvariantCulture, out _wristValue))
-                {
-                    var entry = new ExoEntry
-                    {
-                        Value = _wristValue,
-                        UdpTimestamp = data[2],
-                        UnityTimestamp = unitytime
-                    };
-
-                    OnWristValue.Invoke(entry);
-                }
             }
         }
 
         /// <summary>
-        /// Called to nudge the patien in either exo skeleton directions: up, down, left, right
+        /// Called to nudge the patien in either exo skeleton directions: up, down.
         /// </summary>
-        /// <param name="direction">The direction to nudge in. (up, down, left, right)</param>
+        /// <param name="direction">The direction to nudge in. (up, down)</param>
         public void Nudge(ExoNudgeDir direction)
         {
             Send(direction.ToString());
@@ -73,8 +58,6 @@ namespace Exo
     public enum ExoNudgeDir
     {
         up,
-        down,
-        left,
-        right
+        down
     }
 }
